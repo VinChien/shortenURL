5dde2aa97f798c993fad6dc9cf6d27a151132fd0
vinchiengithub <jianfan0114@gmail.com>
2023-04-06 21:55:12 +0800

    feat : add config

--- app.js
+++ app.js
@@ -2,44 +2,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-// 載入 mongoose
-const mongoose = require('mongoose');
-// 啟用 Handlebars
+
+// 載入 Handlebars
 const exphbs = require('express-handlebars');
-// 啟用 body-parser
+// 載入 body-parser
 const bodyParser = require('body-parser');
+// 載入 mongoose
+require('./config/mongoose');
 
 // 樣板引擎指定為 Handlebars
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
-// 載入 public
+// 載入 public (才能使用 style.css)
 app.use(express.static('public'));
-
 // 每筆請求都要透過 body-parser 作前置處理
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-// 設定連線到 mongoDB，並處理 DeprecationWarning 警告
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// 取得資料庫連線狀態
-const db = mongoose.connection;
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!');
-});
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!');
-});
 
 // 設定首頁路由
 app.get('/', (req, res) => {
