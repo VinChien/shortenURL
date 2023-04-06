affcb354720efdc8858f5edb07215906693f4a5f
vinchiengithub <jianfan0114@gmail.com>
2023-04-06 21:16:40 +0800

    feat : add handlebars

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const app = express();
 const port = 3000;
 // 載入 mongoose
 const mongoose = require('mongoose');
+// 啟用 Handlebars
+const exphbs = require('express-handlebars');
+
+// 樣板引擎指定為 Handlebars
+app.engine(
+  'handlebars',
+  exphbs({ defaultLayout: 'main', extname: '.handlebars' })
+);
+app.set('view engine', 'handlebars');
 
 // 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -29,7 +38,7 @@ db.once('open', () => {
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.render('index');
 });
 
 // 設定 port 3000
